fa6322d33376531270045eb9aa0563d1a539b3c7
JostMK <jost.klink@gmail.com>
2025-04-17 15:47:46 +0200

    Solves exercise 1 problem 5

--- exercise-1/main.cpp
+++ exercise-1/main.cpp
@@ -1,6 +1,8 @@
+#include <charconv>
 #include <iostream>
 #include <filesystem>
 #include <fstream>
+#include <string>
 
 #include "graph.h"
 #include "Stopwatch.h"
@@ -10,26 +12,46 @@ namespace fs = std::filesystem;
 constexpr int DIJKSTRA_ITERATIONS = 100;
 
 const std::string default_graph_file_name = "graph.fmi";
+const std::string default_query_file_name = "queries.txt";
+const std::string default_result_file_name = "result.txt";
+
+static void handle_query_file(std::fstream query_file, std::fstream result_file, const exercise::one::Graph &graph) {
+    auto sw = Stopwatch::Start();
+    std::string line;
+    while (std::getline(query_file, line)) {
+        int start, target;
+        const auto seperator = line.find(' ');
+        std::from_chars(line.data(), line.data() + seperator, start);
+        std::from_chars(line.data() + seperator + 1, line.data() + line.size(), target);
+
+        sw.Restart();
+        const auto dist = graph.compute_shortest_path_dijkstra(start, target);
+        const auto time = sw.Split();
+
+        result_file << start << " " << target << " " << dist << " " << time << "\n";
+    }
+
+    query_file.close();
+    result_file.close();
+}
 
 int main(const int argc, char *argv[]) {
     std::string graph_file_name = default_graph_file_name;
     if (argc > 1) {
         graph_file_name = argv[1];
     }
-
     if (!fs::exists(graph_file_name)) {
-        std::cout << "Couldn't find 'graph.fmi' file!" << std::endl;
+        std::cout << "Couldn't find '" << graph_file_name << "' file!" << std::endl;
         return 1;
     }
-
     std::fstream graph_file(graph_file_name, std::ios::in);
     if (!graph_file.is_open()) {
-        std::cout << "Couldn't open 'graph.fmi' file!" << std::endl;
+        std::cout << "Couldn't open '" << graph_file_name << "' file!" << std::endl;
         return 1;
     }
 
     // problem 1
-    std::cout << "Creating graph from file: " << graph_file_name << std::endl;
+    std::cout << "Creating graph from file: '" << graph_file_name << "'" << std::endl;
     auto sw = Stopwatch::Start();
     const auto graph = exercise::one::Graph(std::move(graph_file));
     std::cout << "Created in " << sw.Stop() << "ms\n" << std::endl;
@@ -52,7 +74,27 @@ int main(const int argc, char *argv[]) {
         const auto dist = graph.compute_shortest_path_dijkstra(start, end);
         overall_time += sw.Split();
     }
-    std::cout << "Computed in average of " << overall_time / DIJKSTRA_ITERATIONS << "ms" << std::endl;
+    std::cout << "Computed in average of " << overall_time / DIJKSTRA_ITERATIONS << "ms\n" << std::endl;
+
+    // problem 5
+    std::string query_file_name = default_query_file_name;
+    if (argc > 2) {
+        query_file_name = argv[2];
+    }
+    if (!fs::exists(query_file_name)) {
+        std::cout << "Couldn't find '" << query_file_name << "' file!" << std::endl;
+        return 1;
+    }
+    std::fstream query_file(query_file_name, std::ios::in);
+    if (!query_file.is_open()) {
+        std::cout << "Couldn't open '" << query_file_name << "' file!" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Processing queries from file: '" << query_file_name << "'" << std::endl;
+    std::fstream result_file(default_result_file_name, std::ios::out);
+    handle_query_file(std::move(query_file), std::move(result_file), graph);
+    std::cout << "Results written to '" << default_result_file_name << "'.\n" << std::endl;
 
     return 0;
 }
