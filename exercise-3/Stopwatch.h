5d3a910050df2dea955c3a67fdd079ce442e2228
JostMK <jost.klink@gmail.com>
2025-06-06 20:09:08 +0200

    [S3E1] Create tasks skeleton

---
+++ exercise-3/Stopwatch.h
@@ -0,0 +1,43 @@
+//
+// Created by Jost on 10/04/2025.
+//
+
+#ifndef STOPWATCH_H
+#define STOPWATCH_H
+
+#include <chrono>
+
+template<class Duration>
+class Stopwatch {
+public:
+    static Stopwatch Start() {
+        Stopwatch stopwatch;
+        stopwatch.begin = std::chrono::steady_clock::now();
+        return stopwatch;
+    }
+
+    long long Split() {
+        end = std::chrono::steady_clock::now();
+        const auto duration = std::chrono::duration_cast<Duration>(end - begin).count();
+        begin = std::chrono::steady_clock::now();
+        return duration;
+    }
+
+    long long Stop() {
+        end = std::chrono::steady_clock::now();
+        return std::chrono::duration_cast<Duration>(end - begin).count();
+    }
+
+    void Restart() {
+        begin = std::chrono::steady_clock::now();
+    }
+
+private:
+    Stopwatch() = default;
+
+    std::chrono::steady_clock::time_point begin{};
+    std::chrono::steady_clock::time_point end{};
+};
+
+
+#endif //STOPWATCH_H
---
+++ exercise-3/main.cpp
@@ -0,0 +1,92 @@
+//
+// Created by Jost on 06/06/2025.
+//
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include <sstream>
+
+#include "Stopwatch.h"
+
+constexpr uint32_t DEFAULT_ELEMENT_COUNT = 1e8;
+
+static std::vector<uint32_t> intersect_naive(const std::vector<uint32_t> &v1, const std::vector<uint32_t> &v2) {
+    return {};
+}
+
+static std::vector<uint32_t> intersect_binary(const std::vector<uint32_t> &v1, const std::vector<uint32_t> &v2) {
+    return {};
+}
+
+static std::vector<uint32_t> intersect_galopping(const std::vector<uint32_t> &v1, const std::vector<uint32_t> &v2) {
+    return {};
+}
+
+static void fill_with_random_numbers(std::vector<uint32_t> &elements, const uint32_t max_value) {
+    std::sort(elements.begin(), elements.end());
+}
+
+int main(const int argc, char *argv[]) {
+    uint32_t element_count_a = DEFAULT_ELEMENT_COUNT;
+    if (argc != 2) {
+        std::cout << "[WARNING] Number of elements not specified! Using default value of " << DEFAULT_ELEMENT_COUNT <<
+                "." << std::endl;
+    } else {
+        const long long value = std::stoll(argv[1]);
+        if (value > std::numeric_limits<uint32_t>::max()) {
+            std::cout <<
+                    "[ERROR] Failed to parse argument, number must be smaller then " << std::numeric_limits<
+                        uint32_t>::max() << "." << std::endl;
+            return 1;
+        }
+        if (value < 0) {
+            std::cout <<
+                    "[ERROR] Failed to parse argument, number must be larger then zero." << std::endl;
+            return 1;
+        }
+        element_count_a = value;
+    }
+
+    std::vector<uint32_t> elements_a(element_count_a);
+    for (uint32_t i = 0; i < element_count_a; i++) {
+        elements_a[i] = i;
+    }
+
+    auto sw = Stopwatch<std::chrono::milliseconds>::Start();
+    const uint32_t element_count_b = std::floor(std::log2(element_count_a));
+    for (uint32_t i = 0; i < element_count_b; i++) {
+        const auto count = static_cast<uint32_t>(static_cast<long double>(element_count_a) / std::pow(2, i));
+        std::vector<uint32_t> elements_b(count);
+        fill_with_random_numbers(elements_b, element_count_a);
+
+        sw.Restart();
+
+        intersect_naive(elements_a, elements_b);
+        const auto naive_time = std::round(sw.Split());
+        std::stringstream naive_time_str;
+        naive_time_str << std::fixed << std::setprecision(2) << naive_time << "ms";
+
+        intersect_binary(elements_a, elements_b);
+        const auto binary_time = std::round(sw.Split());
+        std::stringstream binary_time_str;
+        binary_time_str << std::fixed << std::setprecision(2) << binary_time << "ms";
+
+        intersect_galopping(elements_a, elements_b);
+        const auto galopping_time = std::round(sw.Stop());
+        std::stringstream galopping_time_str;
+        galopping_time_str << std::fixed << std::setprecision(2) << galopping_time << "ms";
+
+        std::cout << "[INFO] "
+                << "Naive: " << std::left << std::setw(12) << naive_time_str.str()
+                << "Binary: " << std::left << std::setw(12) << binary_time_str.str()
+                << "Galopping: " << std::left << std::setw(12) << galopping_time_str.str()
+                << " for " << count << " Elements"
+                << std::endl;
+    }
+
+    return 0;
+}
