58d15abf9a6a75ce315d989b244bb017af9b8087
JostMK <jost.klink@gmail.com>
2025-06-09 00:10:33 +0200

    [S3E1] Replace random fill with sampler

--- exercise-3/main.cpp
+++ exercise-3/main.cpp
@@ -48,53 +48,22 @@ static std::vector<uint32_t> intersect_galopping(const std::vector<uint32_t> &v1
     return {};
 }
 
-static void fill_with_random_numbers(std::vector<uint32_t> &elements, const uint32_t count, const uint32_t max_value) {
-    if (count == max_value) {
-        elements.resize(count);
-        for (uint32_t i = 0; i < max_value; i++) {
-            elements[i] = i;
-        }
-        return;
-    }
-
-    std::vector<uint32_t> rand_values;
-    rand_values.resize(count);
-
-    std::mt19937 rand_alg{1337};
-    std::uniform_int_distribution<uint32_t> distribution{0, max_value - 1};
-
-    for (auto &rand_value: rand_values) {
-        rand_value = distribution(rand_alg);
-    }
-
-    std::sort(rand_values.begin(), rand_values.end());
-
-    elements.reserve(count);
-    uint32_t head = 0;
-    for (const uint32_t value: rand_values) {
-        if (head == 0 || value > elements[head - 1]) {
-            elements.push_back(value);
-            head++;
-        }
-    }
-}
-
 int main(const int argc, char *argv[]) {
     uint32_t element_count_a = DEFAULT_ELEMENT_COUNT;
     if (argc != 2) {
         std::cout << "[WARNING] Number of elements not specified! Using default value of " << DEFAULT_ELEMENT_COUNT <<
-                "." << std::endl;
+                  "." << std::endl;
     } else {
         const long long value = std::stoll(argv[1]);
         if (value > std::numeric_limits<uint32_t>::max()) {
             std::cout <<
-                    "[ERROR] Failed to parse argument, number must be smaller then " << std::numeric_limits<
-                        uint32_t>::max() << "." << std::endl;
+                      "[ERROR] Failed to parse argument, number must be smaller then " << std::numeric_limits<
+                    uint32_t>::max() << "." << std::endl;
             return 1;
         }
         if (value < 0) {
             std::cout <<
-                    "[ERROR] Failed to parse argument, number must be larger then zero." << std::endl;
+                      "[ERROR] Failed to parse argument, number must be larger then zero." << std::endl;
             return 1;
         }
         element_count_a = value;
@@ -110,7 +79,14 @@ int main(const int argc, char *argv[]) {
     for (uint32_t i = 0; i < element_count_b; i++) {
         const auto count = static_cast<uint32_t>(static_cast<long double>(element_count_a) / std::pow(2, i));
         std::vector<uint32_t> elements_b;
-        fill_with_random_numbers(elements_b, count, element_count_a);
+        std::sample(
+                elements_a.begin(),
+                elements_a.end(),
+                std::back_inserter(elements_b),
+                count,
+                std::mt19937{std::random_device{}()}
+        );
+        std::sort(elements_b.begin(), elements_b.end());
 
         sw.Restart();
 
@@ -130,11 +106,11 @@ int main(const int argc, char *argv[]) {
         galopping_time_str << std::fixed << std::setprecision(2) << galopping_time << "ms";
 
         std::cout << "[BENCHMARK] "
-                << "Naive: " << std::left << std::setw(12) << naive_time_str.str()
-                << "Binary: " << std::left << std::setw(12) << binary_time_str.str()
-                << "Galopping: " << std::left << std::setw(12) << galopping_time_str.str()
-                << " for " << elements_b.size() << " Elements"
-                << std::endl;
+                  << "Naive: " << std::left << std::setw(12) << naive_time_str.str()
+                  << "Binary: " << std::left << std::setw(12) << binary_time_str.str()
+                  << "Galopping: " << std::left << std::setw(12) << galopping_time_str.str()
+                  << " for " << elements_b.size() << " Elements"
+                  << std::endl;
     }
 
     return 0;
